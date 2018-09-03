663eb2e2d22e68072567beaac2827878c546fd0c
Rudy Ravelin <rudy@noukod.com>
2018-09-03 17:49:42 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,6 +5,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 def valid_move?(board, position)
   if position.to_i.between?(0,8)
     if !position_taken?(board, position.to_i-1)
