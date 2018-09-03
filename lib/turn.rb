7a0fbd08bf547041ab446df0cd69213e15ec5438
Rudy Ravelin <rudy@noukod.com>
2018-09-03 17:53:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,36 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, index)
-  # if index.between?(1,9)
-  #   if !position_taken?(board, index)
-  #     true
-  #   end
-  # end
-
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def position_taken?(board, index)
-  board[index] != " "
-end
+def valid_move?(board, position)
+  if position.to_i.between?(0,8)
+    if !position_taken?(board, position.to_i-1)
+      true
+    end
+  end
   # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
-  def position_taken?(board, index)
-  # basic solution:
-
-  # taken = nil
-  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-  #   taken = false
-  # else
-  #   taken = true
-  # end
-  # taken
-
-  # advanced solution w/ ternary operator
-  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
-    false : true
-
-end
 
 def turn(board)
   puts "Please enter 1-9:"
